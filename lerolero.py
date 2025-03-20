aa3e8d619d541e2a10c97365d95a8e9fe5aabbcc
Alejandro Larrea <1alejandro220@usp.br>
2025-03-20 10:38:39 -0300

    mudança nos comentarios

--- lerolero.py
+++ lerolero.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-***Gerador de lero-lero.
+#Gerador de lero-lero.
 
-Gera frases de efeito sem significado real.***
+#Gera frases de efeito sem significado real.
 
 import random
 
